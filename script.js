91519e1ed6f3055204bdafb9c9b3bd5cf5f74703
mgporter <26415062+mgporter@users.noreply.github.com>
2023-06-06 16:53:12 -0400

    javascript functionality mostly completed

--- script.js
+++ script.js
@@ -4,18 +4,28 @@ const addBookForm = document.getElementById('add-book-form')
 const submitBtn = document.getElementById('formsubmit');
 const contentContainer = document.getElementById('content-container');
 
-const statusDropdownMenu = document.querySelectorAll('.status-dropdown-item')
-
-statusDropdownMenu.forEach((item) => {
-    item.addEventListener('mouseover', (e) => {
-        console.log(`${this} and ${e}`)
-        item.style.backgroundColor = 'red';
-    })
-})
-
 let deleteBookBtns;
+let statusButtons;
+let statusDropdownMenus;
 
 let myLibrary = [];
+let numberOfBooksPerRow = 5;
+
+
+
+
+const changeReadStatus = function(e) {
+    // get the value of readstatus from the child element (the one selected by the user)
+    const readStatus = this.lastChild.dataset.readstatus;
+
+    const bookNum = this.dataset.book;
+    const readIndicator = document.querySelector(`.book-content.status-color[data-book="${bookNum}"]`)
+    
+    // set the read status indicator to the selected response
+    readIndicator.dataset.readstatus = readStatus;
+
+    toggleVisibility(document.querySelector(`div.status-dropdown[data-book="${bookNum}"]`))
+}
 
 
 
@@ -32,60 +42,15 @@ function Book(title, author, pagecount, readStatus) {
     this.readStatus = readStatus;
 }
 
-// add book object to array
+
+// add book object to array and redraw library
 function addBookToLibrary(title, author, pagecount, readStatus) {
     const myBook = new Book(title, author, pagecount, readStatus);
     myLibrary.push(myBook);
 
     displayLibrary()
-    
 }
 
-function displayLibrary() {
-    // loop through myLIbrary and display on the page
-
-    // while (contentContainer.lastChild) {
-    //     contentContainer.removeChild(contentContainer.lastChild)
-    // }
-
-    let numBooksPerRow = 3;
-
-    for (let i = 0; i < (myLibrary.length / numBooksPerRow); i++) {
-
-        const libraryRow = document.createElement('div')
-        libraryRow.classList.add('library-row', `row-${i + 1}`)
-
-
-        for (let j = 0; j < numBooksPerRow; j++) {
-
-            if (myLibrary[j+(i*numBooksPerRow)]) {
-                const bookContainer = document.createElement('div')
-                bookContainer.classList.add('book-container')
-                bookContainer.innerHTML = 
-                    `<img src="./spotlight3.png" alt="">
-                    <div class="book">
-                        <div class="book-content-container">
-                            <button class="delete-book-btn" data-book="${j+(i*numBooksPerRow)}"></button>
-                            <div class="book-content title"><h3>${myLibrary[j+(i*numBooksPerRow)].title}</h3></div>
-                            <h4 class="book-content author">${myLibrary[j+(i*numBooksPerRow)].author}</h4>
-                            <h4 class="book-content pages">${myLibrary[j+(i*numBooksPerRow)].pagecount}</h4>
-                            <h4 class="book-content status">Status: ${myLibrary[j+(i*numBooksPerRow)].readStatus}</h4>
-                        </div>
-                    </div>`
-                
-                libraryRow.appendChild(bookContainer)
-            }
-        }
-        contentContainer.appendChild(libraryRow)
-        deleteBookBtns = document.querySelectorAll('button.delete-book-btn');
-
-    }
-
-    // add the event listeners to the newly rendered books
-    deleteBookBtns.forEach((item) => {
-        item.addEventListener('click', deleteBook)
-    })
-}
 
 // get the delete button's data-book value and remove that book from the array
 const deleteBook = function(e) {
@@ -94,28 +59,114 @@ const deleteBook = function(e) {
 }
 
 
+ // loop through myLIbrary and display on the page
+function displayLibrary() {
+   
+    // first, we remove all elements in the library display window, called contentContainer
+    while (contentContainer.lastChild) {
+        contentContainer.removeChild(contentContainer.lastChild)
+    }
 
-const toggleAddBookDialog = function(e) {
-    let changeTo = '';
-    
-    if (addBookDialogBox.classList.contains('hidden')) {
-        changeTo = 'visible';
-        addBookDialogBox.classList.remove('hidden')
-     } else {
-        changeTo = 'hidden';
-        addBookDialogBox.classList.remove('visible')
-     }
+    // initialize the number of books per row variable. This is not really necessary
+    const bpr = numberOfBooksPerRow;
 
-    addBookDialogBox.style.visibility = changeTo;
-    addBookDialogBox.classList.add(changeTo)
+    // the outer loop draws the rows
+    for (let i = 0; i < (myLibrary.length / bpr); i++) {
+
+        const libraryRow = document.createElement('div')
+        libraryRow.classList.add('library-row', `row-${i + 1}`)
+
+        // the inner loop draws each book element
+        for (let j = 0; j < bpr; j++) {
+
+            const bookIndex = j + (i * bpr)
+
+            // the inner loop will draw a maximum of 'bpr' books per row, but if there
+            // aren't enough books to fill out the last row, this check will return false
+            if (myLibrary[bookIndex]) {
+                
+                const bookContainer = document.createElement('div')
+                bookContainer.classList.add('book-container')
+                bookContainer.setAttribute('data-book', bookIndex)
+                
+                // adding the html
+                bookContainer.innerHTML = `
+                <img data-book="${bookIndex}" src="./spotlight3.png" alt="">
+                <div class="book">
+                    <div class="book-content-container">
+                        <button data-book="${bookIndex}" class="delete-book-btn"></button>
+                        <div class="book-content title"><h3>${myLibrary[bookIndex].title}</h3></div>
+                        <h4 class="book-content author">${myLibrary[bookIndex].author}</h4>
+                        
+                        <div class="book-content status">
+                            <h4 class="book-content pages">${myLibrary[bookIndex].pagecount}</h4>
+                            <div data-book="${bookIndex}" class="book-content status-button">
+                                <div data-book="${bookIndex}" data-readstatus="${myLibrary[bookIndex].readStatus}" class="book-content status-color"></div>
+                            </div>
+                            <div data-book="${bookIndex}" class="status-dropdown hidden">
+                                <div data-book="${bookIndex}" class="status-dropdown-item">unread<div data-readstatus="unread" class="book-content status-color"></div></div>
+                                <div data-book="${bookIndex}" class="status-dropdown-item">in progress<div data-readstatus="currently" class="book-content status-color"></div></div>
+                                <div data-book="${bookIndex}" class="status-dropdown-item">finished<div data-readstatus="finished" class="book-content status-color"></div></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>`
+
+                libraryRow.appendChild(bookContainer)
+            }
+        }
+        contentContainer.appendChild(libraryRow)
+
+    }
+
+    // after the loops finish, we'll create and append one last row
+    const libraryRow = document.createElement('div')
+    libraryRow.classList.add('library-row', `row-final`)
+    contentContainer.appendChild(libraryRow)
+
+    // grab the newly created elements
+    deleteBookBtns = document.querySelectorAll('button.delete-book-btn');
+    statusButtons = document.querySelectorAll('.book-content.status-button');
+    statusDropdownMenus = document.querySelectorAll('.status-dropdown-item');
+
+    // then add the event listeners to the newly rendered books
+    deleteBookBtns.forEach((item) => {
+        item.addEventListener('click', deleteBook)
+    })
+
+    statusButtons.forEach((item) => {
+        item.addEventListener('click', function(e) {
+            const bookNum = this.dataset.book;
+            const dropdownElement = document.querySelector(`div.status-dropdown[data-book="${bookNum}"]`)
+            toggleVisibility(dropdownElement)
+        })
+    })
+
+    statusDropdownMenus.forEach((item) => {
+        item.addEventListener('click', changeReadStatus)
+    })
 }
 
-addBookButton.addEventListener('click', toggleAddBookDialog);
 
-// submitBtn.addEventListener('click', (e) => {
-//     e.preventDefault;
-// })
+// toggles visibility on all menu elements
+const toggleVisibility = function(element) {
+    
+    if (element.className.includes('hidden')) {
+        element.classList.remove('hidden');
+        element.classList.add('visible');
+    } else {
+        element.classList.remove('visible');
+        element.classList.add('hidden');
+    }
+}
 
+
+addBookButton.addEventListener('click', () => {
+    toggleVisibility(addBookDialogBox)
+});
+
+
+// gets the book information from the add book form, then calls the addBookTolibrary function
 addBookForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const title = e.target[0].value
@@ -128,35 +179,36 @@ addBookForm.addEventListener('submit', (e) => {
 }, false)
 
 
-books = [
+// dummy books
+let books = [
     {
         'title': 'The Great Gatsby',
         'author': 'F. Scott Fitzgerald',
-        'pagecount': 378,
+        'pagecount': '378 pages',
         'readStatus': 'unread',
     },
     {
         'title': 'To Kill A Mockingbird',
         'author': 'Harper Lee',
-        'pagecount': 298,
-        'readStatus': 'unread',
+        'pagecount': '298 pages',
+        'readStatus': 'finished',
     },
     {
         'title': 'Moby-Dick',
         'author': 'Herman Melville',
-        'pagecount': 514,
+        'pagecount': '514 pages',
         'readStatus': 'unread',
     },
     {
         'title': 'Pride and Prejudice',
         'author': 'Jane Austen',
-        'pagecount': 371,
-        'readStatus': 'unread',
+        'pagecount': '371 pages',
+        'readStatus': 'currently',
     },
     {
         'title': 'Adventures of Huckleberry Finn',
         'author': 'Mark Twain',
-        'pagecount': 213,
+        'pagecount': '213 pages',
         'readStatus': 'unread',
     },
 ]
@@ -164,14 +216,5 @@ books = [
 books.forEach((book) => {
     myLibrary.push(book)
 })
+
 displayLibrary()
-
-// Add a “NEW BOOK” button that brings up a form allowing users to input the details 
-// for the new book: author, title, number of pages, whether it’s been read and anything 
-// else you might want.
-
-// Add a button on each book’s display to remove the book from the library.
-
-// Add a button on each book’s display to change its read status.
-// To facilitate this you will want to create the function that toggles a 
-// book’s read status on your Book prototype instance.
